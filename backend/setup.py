7f785fca65048ac4c897879ba9bb09d2bdb20472
Fosten <fosten@gmail.com>
2026-05-19 13:51:34 -0400

    Bump Plone from 6.2.0rc2 to 6.2.0

--- backend/setup.py
+++ backend/setup.py
@@ -23,7 +23,6 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
-        "Framework :: Plone :: 6.1",
         "Framework :: Plone :: 6.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.10",
